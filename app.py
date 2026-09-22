f076c73d87c8f40dca463bfe53da9fa92630ca96
June <june@macbook.local>
2026-09-22 02:13:42 -0400

    added app.py

---
+++ app.py
@@ -0,0 +1,28 @@
+from letterboxdpy.list import List
+import random
+import streamlit as st
+
+st.title("Letterboxd Random Movie Gen")
+
+username = st.text_input("Enter Letterboxd username: ")
+list = st.text_input("Enter the list name (copy after /list/ in url): ")
+
+if st.button("Random Movie"):
+    if username and list:
+        try:
+            with st.spinner(f"Grabbing movies from {username}'s list..."):
+                list_instance = List(username, list)
+                movie_list = []
+                for movie_id, movie in list_instance.movies.items():
+                    if "name" in movie:
+                        title_year = f"{movie['name']} ({movie.get('year', 'N/A')})"
+                        movie_list.append(title_year)
+            if len(movie_list) > 0:
+                selected_movie = random.choice(movie_list)
+                st.success(f"Selected Movie: {selected_movie}")
+            else:
+                st.warning("Couldn't find movies/list")
+        except Exception as e:
+            st.error(f"Error grabbing list: {e}")
+    else: 
+        st.warning("Fill in all fields")
