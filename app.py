7498fbaa1a07571bbad6516c50d4cc0d49f581fc
June <june@macbook.local>
2026-09-23 22:33:28 -0400

    bug fixing

--- app.py
+++ app.py
@@ -19,7 +19,7 @@ def list_grabber():
                     if "name" in movie:
                         title_year = f"{movie['name']} ({movie.get('year', 'N/A')})"
                         movie_list.append(title_year)
-            if len((movie_list) > 0):
+            if (len(movie_list) > 0):
                 selected_movie = random.choice(movie_list)
                 st.success(f"Selected Movie: {selected_movie}")
             else:
@@ -36,7 +36,7 @@ def watchlist_grabber():
                     if "name" in movie:
                         title_year = f"{movie['name']} ({movie.get('year', 'N/A')})"
                         movie_list.append(title_year)
-            if len((movie_list) > 0):
+            if (len(movie_list) > 0):
                 selected_movie = random.choice(movie_list)
                 st.success(f"Selected Movie: {selected_movie}")
             else:
